daf8aca8a93a39b048715437d59fb9cd083fb6bc
joma74 <dev-mails@gmx.net>
2016-03-19 22:05:15 +0100

    Initial Module Setup

---
+++ component/impl/src/main/java/at/joma/apidesign/component/impl/App.java
@@ -0,0 +1,13 @@
+package at.joma.apidesign.component.impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
