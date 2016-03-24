72560ef20d696827b825ef2417e4dc377f2c461e
joma74 <dev-mails@gmx.net>
2016-03-24 22:18:20 +0100

    Fixes #2 Added impl-one and impl-two

---
+++ component/impl-two/src/main/java/at/joma/apidesign/component/impl/ComponentImpl.java
@@ -0,0 +1,13 @@
+package at.joma.apidesign.component.impl;
+
+import at.joma.apidesign.component.api.IComponent;
+
+public class ComponentImpl implements IComponent{
+
+	@Override
+	public void doSomething() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
