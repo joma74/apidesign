2a7b3efd7ef8b4111a583fd1b712708951e82dee
joma74 <dev-mails@gmx.net>
2016-03-31 23:10:39 +0200

    Renamed top-level-api to l1-client-api Added l2-client-api

---
+++ component/l2-client-api/src/main/java/at/joma/apidesign/l2/client/api/App.java
@@ -0,0 +1,13 @@
+package at.joma.apidesign.l2.client.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
