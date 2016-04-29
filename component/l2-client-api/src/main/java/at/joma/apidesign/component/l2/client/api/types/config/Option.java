46930fb490890f174f622b27175192dff1064313
Jörg Maurer <joerg.maurer@itsv.at>
2016-04-29 10:53:28 +0200

    Refactored ERR Messages and added Tests for Options as Class Array

--- component/l2-client-api/src/main/java/at/joma/apidesign/component/l2/client/api/types/config/Option.java
+++ component/l2-client-api/src/main/java/at/joma/apidesign/component/l2/client/api/types/config/Option.java
@@ -14,16 +14,26 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.helpers.MessageFormatter;
 
 import at.joma.apidesign.component.l1.client.api.config.IOption;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Collections2;
 
 public class Option implements IOption, Serializable {
 
     private static final long serialVersionUID = 5979614171013169677L;
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(Option.class);
+
+    public static final String ERR_OPTIONVALUE_NULL = "optionValue may not be null. optionName was >>{}<<.)";
+    
+    public static final String ERR_OPTIONVALUES_NULL = "optionValues of >>{}<< may not contain null values. optionName was >>{}<<.";
+    
+    public static final String ERR_OPTIONNAME_NULL = "optionName may not be null";
+    
 
     public enum OptionType {
         ENUM(Enum.class),
@@ -56,36 +66,46 @@ public class Option implements IOption, Serializable {
     }
 
     public Option(String optionName, String[] optionValues) {
-        Preconditions.checkNotNull(optionName);
-        Preconditions.checkNotNull(optionValues);
+        checkOptionPreconditions(optionName, optionValues);
         this.name = optionName;
         this.value = Arrays.toString(optionValues);
         this.type = OptionType.STRINGARRAY;
     }
 
     public Option(String optionName, String optionValue) {
-        Preconditions.checkNotNull(optionName);
-        Preconditions.checkNotNull(optionValue);
+        checkOptionPreconditions(optionName, optionValue);
         this.name = optionName;
         this.value = optionValue;
         this.type = OptionType.STRING;
     }
-    
+
     public Option(String optionName, Class<?>[] optionValues) {
-        Preconditions.checkNotNull(optionName);
-        Preconditions.checkNotNull(optionValues);
+        checkOptionPreconditions(optionName, optionValues);
         this.name = optionName;
         Set<String> intermediate = new HashSet<>(optionValues.length);
-        for(Class<?> optionValue : optionValues){
+        for (Class<?> optionValue : optionValues) {
             intermediate.add(optionValue.getName());
         }
         this.value = Arrays.toString(intermediate.toArray());
         this.type = OptionType.CLAZZARRAY;
     }
 
+    private void checkOptionPreconditions(String optionName, Object optionValue) {
+        Preconditions.checkNotNull(ERR_OPTIONNAME_NULL, optionName);
+        Preconditions.checkNotNull(MessageFormatter.format(ERR_OPTIONVALUE_NULL, optionName).getMessage(), optionValue);
+    }
+
+    private void checkOptionPreconditions(String optionName, Object[] optionValues) {
+        Preconditions.checkNotNull(ERR_OPTIONNAME_NULL, optionName);
+        Preconditions.checkNotNull(optionValues);
+        if (!Collections2.filter(Arrays.asList(optionValues), Predicates.isNull()).isEmpty()) {
+            throw new NullPointerException(MessageFormatter.format(ERR_OPTIONVALUES_NULL, Arrays.toString(optionValues), optionName).getMessage());
+        }
+    }
+
     public String[] convertValueToArray() {
         String joinedMinusBrackets = this.value.substring(1, this.value.length() - 1);
-        if(joinedMinusBrackets.isEmpty()){
+        if (joinedMinusBrackets.isEmpty()) {
             return new String[]{};
         }
         return joinedMinusBrackets.split(", ");
@@ -104,7 +124,7 @@ public class Option implements IOption, Serializable {
     public String getValue() {
         return this.value;
     }
-    
+
     @Override
     public IOption deepClone() {
         try {
--- component/l2-client-api/src/test/java/at/joma/apidesign/component/l2/client/api/types/config/OptionTest.java
+++ component/l2-client-api/src/test/java/at/joma/apidesign/component/l2/client/api/types/config/OptionTest.java
@@ -5,11 +5,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Arrays;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.slf4j.helpers.MessageFormatter;
 
 import at.joma.apidesign.component.l2.client.api.types.SortingDirection;
 
 public class OptionTest {
+    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void testOptionSupportForEnum() {
@@ -43,6 +49,67 @@ public class OptionTest {
         assertThat(option.getName(), equalTo(OPTION_NAME));
         assertThat(option.convertValueToArray(), equalTo(OPTION_VALUE));
         assertThat(option.toString(), equalTo("Option[" + OPTION_NAME + ", " + Arrays.toString(OPTION_VALUE) + "]"));
+    }
+    
+    
+    @Test
+    public void testOptionSupportForArrayClass() {
+        String OPTION_NAME = "OPTION_NAME";
+        Class<?>[] OPTION_VALUE = new Class[]{
+            Object.class,
+        };
+        Option option = new Option(OPTION_NAME, OPTION_VALUE);
+        assertThat(option.getType(), equalTo(Option.OptionType.CLAZZARRAY));
+        assertThat(option.getName(), equalTo(OPTION_NAME));
+        assertThat(option.convertValueToArray(), equalTo(new String[]{Object.class.getName()}));
+        assertThat(option.toString(), equalTo("Option[" + OPTION_NAME + ", [" + Object.class.getName() + "]]"));
+        
+    }
+    
+    @Test
+    public void testOptionEmptyArrayString() {
+        String OPTION_NAME = "OPTION_NAME";
+        String[] OPTION_VALUE = new String[]{};
+        Option option = new Option(OPTION_NAME, OPTION_VALUE);
+        assertThat(option.getType(), equalTo(Option.OptionType.STRINGARRAY));
+        assertThat(option.getName(), equalTo(OPTION_NAME));
+        assertThat(option.convertValueToArray(), equalTo(OPTION_VALUE));
+        assertThat(option.toString(), equalTo("Option[" + OPTION_NAME + ", " + Arrays.toString(OPTION_VALUE) + "]"));
+    }
+    
+    @Test
+    public void testOptionEmptyArrayClass() {
+        String OPTION_NAME = "OPTION_NAME";
+        Class<?>[] OPTION_VALUE = new Class[]{};
+        Option option = new Option(OPTION_NAME, OPTION_VALUE);
+        assertThat(option.getType(), equalTo(Option.OptionType.CLAZZARRAY));
+        assertThat(option.getName(), equalTo(OPTION_NAME));
+        assertThat(option.convertValueToArray(), equalTo(new String[]{}));
+        assertThat(option.toString(), equalTo("Option[" + OPTION_NAME + ", []]"));
+    }
+    
+    @Test
+    public void testOptionNullArrayString() {
+        
+        String OPTION_NAME = "OPTION_NAME";
+        String[] OPTION_VALUE = new String[1];
+        
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage(MessageFormatter.format(Option.ERR_OPTIONVALUES_NULL, Arrays.toString(OPTION_VALUE), OPTION_NAME).getMessage());
+        
+        new Option(OPTION_NAME, OPTION_VALUE);
+    }
+    
+    @Test
+    public void testOptionNullArrayClass() {
+        
+        String OPTION_NAME = "OPTION_NAME";
+        Class<?>[] OPTION_VALUE = new Class[1];
+        
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage(MessageFormatter.format(Option.ERR_OPTIONVALUES_NULL, Arrays.toString(OPTION_VALUE), OPTION_NAME).getMessage());
+        
+        new Option(OPTION_NAME, OPTION_VALUE);
     }
 
 }
